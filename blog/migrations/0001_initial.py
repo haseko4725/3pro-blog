1533467b49260a4234bef01de5307e90621725ff
haseko4725 <g20908hm@gm.tsuda.ac.jp>
2022-10-25 05:03:39 +0900

    commit

--- blog/migrations/0001_initial.py
+++ blog/migrations/0001_initial.py
@@ -12,7 +12,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Post',
+            name='Fitnessgraph',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('active', models.CharField(max_length=100)),
