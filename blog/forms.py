a12cdb484e5cba600d038a4dced9fe023c04b23c
haseko4725 <g20908hm@gm.tsuda.ac.jp>
2022-10-25 14:30:10 +0900

    commit

--- blog/forms.py
+++ blog/forms.py
@@ -5,25 +5,10 @@ from blog.models import Fitnessgraph
 
 
 class Fitnessform(forms.Form):
-    active = forms.fields.ChoiceField(
-        choices = (
-            ('',''),
-            (3.0, 'バレーボール'),
-            (3.5, 'ゴルフ'),
-            (4.0, '卓球'),
-            (4.5, 'バドミントン'),
-            (5.0, '野球'),
-            (6.0, 'バスケットボール'),
-            (7.0, 'サッカー'),
-            (8.0, 'ランニング'),
-        ),
-        initial=10,
-        required=True,
-        widget=forms.widgets.Select()
-    )
+    active = forms.CharField(max_length = 100, label="active")
 
-    time = forms.IntegerField(label="時間")
-    day = forms.DateField(label="日付")
+    time = forms.IntegerField(label="time")
+    day = forms.DateField(label="day")
     
     def save(self):
         data = self.cleaned_data
