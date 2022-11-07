8a1b49d5d1f601c750b9e7a0b4733a49b4bb4e94
haseko4725 <g20908hm@gm.tsuda.ac.jp>
2022-11-07 13:04:08 +0900

    commit

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,17 @@ from email.policy import default
 from django.db import models
 
 class Fitnessgraph(models.Model):
-    active = models.CharField(max_length=100)
+    class ActiveMetz():
+        metz3 = 3
+        metz4 = 4
+        metz5 = 5
+    ACTIVE_CHOICE = (
+        (ActiveMetz.metz3,'体操'),
+        (ActiveMetz.metz4,'卓球'),
+        (ActiveMetz.metz5,'野球'),
+    )
+    active = models.CharField(choices=ACTIVE_CHOICE,
+                            max_length=100,)
     time = models.IntegerField(blank=True, null=True)
     day = models.DateField()
 
