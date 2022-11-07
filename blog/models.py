fe73b62f9e10c700dd8f5770016ce28ce22d04fd
haseko4725 <g20908hm@gm.tsuda.ac.jp>
2022-11-07 13:49:26 +0900

    commit

--- blog/models.py
+++ blog/models.py
@@ -1,24 +1,11 @@
 from email.policy import default
 from django.db import models
 
+class Fitnesspulldown(models.Model):
+    active = models.CharField(max_length=100,blank=True)
+    metz = models.FloatField(default=0)
+
 class Fitnessgraph(models.Model):
-    class ActiveMetz():
-        metz3 = 3
-        metz4 = 4
-        metz5 = 5
-    ACTIVE_CHOICE = (
-        (ActiveMetz.metz3,'体操'),
-        (ActiveMetz.metz4,'卓球'),
-        (ActiveMetz.metz5,'野球'),
-    )
-    active = models.CharField(choices=ACTIVE_CHOICE,
-                            max_length=100,)
+    active = models.ForeignKey(Fitnesspulldown, on_delete=models.SET_NULL, null=True)
     time = models.IntegerField(blank=True, null=True)
     day = models.DateField()
-
-    def __str__(self):
-        return self.active
-
-class Fitnesspulldown(models.Model):
-    active = models.CharField(max_length=100)
-    metz = models.FloatField(default=0)
